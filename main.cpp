30888854b73a33fd902e35a7241e1e92848cf247
Adam Dziwiszek <adziwiszek1@gmail.com>
2024-12-05 13:49:45 +0100

    working bezier curves!

--- main.cpp
+++ main.cpp
@@ -1,38 +1,73 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include <memory>
+#include <cmath>
 #include <iostream>
 
 using Vec2f = sf::Vector2f;
 using std::unique_ptr, std::vector;
 
+// declarations
+class Point;
+std::vector<Point> graham_scan(std::vector<Point> points);
+float bernstein_poly(int n, int k, float x);
+Point bezier_point(float t, vector<Point> const &chull);
+
 class Point {
 protected:
-  sf::RectangleShape representation;
+  sf::CircleShape representation;
   Vec2f position;
-  Vec2f size;
+  float radius;
 
 public:
+  float x;
+  float y;
+
   Point(Vec2f _position)
-    : position { _position }
+    : position { _position }, x { _position.x }, y { _position.y }
   {
-    size = Vec2f(10.0f, 10.0f);
-    representation = sf::RectangleShape(size);
+    radius = 10.0f;
+    representation = sf::CircleShape(radius);
+    representation.setOrigin({10.0f, 10.0f});
     representation.setPosition(position);
-    representation.setFillColor(sf::Color::Blue);
+    //representation.setFillColor(sf::Color::Blue);
     representation.setOutlineColor(sf::Color::Red);
-    representation.setOutlineThickness(2.0f);
+    representation.setOutlineThickness(1.0f);
+  }
+
+  Point operator*(float const t) {
+    return Point( { x * t, y * t} );
+  }
+
+  Point operator+(Point const& p) {
+    return Point( {x + p.x, y + p.y} );
+  }
+
+  // linear interpolation
+  static Point lerp(const Point& p1, const Point& p2, float t) {
+    return Point({
+      p1.x + t * (p2.x - p1.x),
+      p1.y + t * (p2.y - p1.y)
+    });
+  }
+
+
+
+  float dist_from_point(Vec2f pos) {
+    return std::sqrt(std::pow(x - pos.x, 2) + std::pow(y - pos.y, 2));
   }
   
   bool mouse_in(Vec2f mpos) {
-    if(mpos.x >= position.x && mpos.x <= position.x + size.x &&
-       mpos.y >= position.y && mpos.y <= position.y + size.y)
+    if(dist_from_point(mpos) < radius) {
       return true;
+    }
     return false;
   }
 
   void update_position(Vec2f pos) {
     position = pos;
+    x = pos.x;
+    y = pos.y;
   }
 
   void draw(sf::RenderWindow *window) {
@@ -47,20 +82,95 @@ public:
     points.push_back(Point(pos));
   }
 
+  // De Casteljau algorithm for Bézier curve point calculation
+  static Point deCasteljau(std::vector<Point> controlPoints, float t) {
+    // Create a copy of control points to work with
+    std::vector<Point> points = controlPoints;
+    //std::cout << "n = " << points.size() << std::endl;
+    for (size_t r = 1; r < controlPoints.size(); ++r) {
+      for (size_t i = 0; i < controlPoints.size() - r; ++i) {
+        // Linear interpolation between adjacent points
+        points[i] = points[i] * (1 - t) + points[i + 1] * t;
+      }
+    }
+    //std::cout << "x = " << points[0].x << " | y = " << points[0].y << " | t = " << t << std::endl;
+
+    // The final point is the point on the Bézier curve
+    return points[0];
+  }
+
   void draw_points(sf::RenderWindow *window) {
     for(auto p: points) {
       p.draw(window);
     }
   }
 
-  Point* mouse_over_point(Vec2f mpos) {
-    for(size_t i = 0; i < points.size(); i++){ 
-      Point* p = &points[i];
-      if(p->mouse_in(mpos)) {
-        return p;
-      }
+  Point& get_point(size_t index) {
+    // TODO: handle wrong index
+    // if(index >= points.size()) 
+    return points[index];
+  }
+
+  size_t points_count() const {
+    return points.size();
+  }
+
+  void draw_convex_hull(sf::RenderWindow *window) {
+    // If convex hull is to small we dont draw it
+    if(convex_hull.size() < 3) 
+      return;
+
+    for (size_t i = 0; i < convex_hull.size(); ++i) {
+      // linia od od itego do i+1 wierzcholka otoczki wypuklej
+      sf::Vertex line[] = {
+        sf::Vertex(sf::Vector2f(convex_hull[i].x, convex_hull[i].y), sf::Color::Blue),
+        sf::Vertex(sf::Vector2f(convex_hull[(i+1) % convex_hull.size()].x,
+              convex_hull[(i+1) % convex_hull.size()].y), sf::Color::Blue)
+      };
+      window->draw(line, 2, sf::Lines);
     }
-    return NULL;
+  }
+
+  void draw_bezier_lines(sf::RenderWindow *window) {
+    if(convex_hull.size() < 3) 
+      return;
+
+    for (size_t i = 0; i < bezier_points.size() - 1; ++i) {
+      //std::cout << "i = " << i << std::endl;
+      //std::cout << bezier_points[i].x << " " << bezier_points[i].y << std::endl;
+      // linie do zrobienia iluzji krzywej beziera
+      sf::Vertex line[] = {
+        sf::Vertex(sf::Vector2f(bezier_points[i].x, bezier_points[i].y), sf::Color::Green),
+        sf::Vertex(sf::Vector2f(bezier_points[i + 1].x, bezier_points[i + 1].y), sf::Color::Green)
+      };
+      window->draw(line, 2, sf::Lines);
+    }
+  }
+
+  vector<Point> generate_curve_points(int n) {
+    vector<Point> res;
+    if(points.size() < 3)
+      return res;
+
+    for(int i = 0; i <= n; i++) {
+      //std::cout << i << std::endl;
+      float t = static_cast<float>(i) / static_cast<float>(n);
+      res.push_back(deCasteljau(points, t));
+    }
+    return res;
+  }
+
+  size_t mouse_over_point(Vec2f mpos) {
+    for(size_t i = 0; i < points.size(); i++){ 
+      if(points[i].mouse_in(mpos))
+        return i;
+    }
+    return SIZE_MAX;
+  }
+
+  void update() {
+    convex_hull = graham_scan(points);
+    bezier_points = generate_curve_points(20);
   }
 
   void undo_last_point() {
@@ -71,6 +181,8 @@ public:
 
 private:
   vector<Point> points;
+  vector<Point> convex_hull;
+  vector<Point> bezier_points;
 };
 
 class User {
@@ -91,12 +203,19 @@ public:
   void handle_input(sf::Event event, Vec2f mpos) {
     if (event.type == sf::Event::MouseButtonPressed) {
       if (event.mouseButton.button == sf::Mouse::Left) {
-        active_point = active_curve->mouse_over_point(mpos);
-        if(active_point == NULL) { // we are not mousing over a point
-          if(!lmb_pressed) { // if we are not mousing over we add a point
-            active_curve->spawn_point(mpos);
-          }
-        }         
+        size_t point_index = active_curve->mouse_over_point(mpos);
+
+        if (point_index != SIZE_MAX) {
+          active_point = &(active_curve->get_point(point_index));
+        } else if (!lmb_pressed) {
+          // adds a new point and sets it as an active point
+          active_curve->spawn_point(mpos);
+          active_point = &(active_curve->get_point(active_curve->points_count() - 1));
+        }
+        /*// we check if we found an active point
+        if(!lmb_pressed && active_point == NULL) {
+          active_curve->spawn_point(mpos);
+        }*/
         lmb_pressed = true;
       }
     }
@@ -126,20 +245,99 @@ public:
         active_point->update_position(mpos);
       }
     }
+
+    for(auto& curve: curves) {
+      curve.update();
+    }
   }
 
-  void drawCurvePoints(sf::RenderWindow *window) {
+  void draw_curve_points(sf::RenderWindow *window) {
     for(auto curve: curves) {
       curve.draw_points(window);
     }
   }
+
+  void draw_convex_hull(sf::RenderWindow *window) {
+    for(auto curve: curves) {
+      curve.draw_convex_hull(window);
+    }
+  }
+
+  void draw_bezier_curve(sf::RenderWindow *window) {
+    for(auto curve: curves) {
+      curve.draw_bezier_lines(window);
+    }
+  }
   
+  void draw(sf::RenderWindow *window) {
+    draw_curve_points(window);
+    draw_convex_hull(window);
+    draw_bezier_curve(window);
+  }
 };
+
+float orientation(const Point& p, const Point& q, const Point& r) {
+    return (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
+}
+
+float distance(const Point& p1, const Point& p2) {
+    return std::sqrt(std::pow(p1.x - p2.x, 2) + std::pow(p1.y - p2.y, 2));
+}
+
+std::vector<Point> graham_scan(std::vector<Point> points) {
+    //  Jeśli mamy mniej niż 3 punkty, nie możemy utworzyć otoczki
+    if (points.size() < 3) return points;
+
+    //  Znajdź punkt o najmniejszej współrzędnej y 
+    // (w przypadku równych y - punkt o najmniejszym x)
+    auto minYPoint = std::min_element(points.begin(), points.end(), 
+        [](const Point& a, const Point& b) {
+            return a.y < b.y || (a.y == b.y && a.x < b.x);
+        });
+
+    //  Przesuń punkt o najmniejszej współrzędnej y na początek wektora
+    std::swap(*minYPoint, points[0]);
+    Point& pivot = points[0];
+
+    //  Sortuj punkty względem kąta z punktem bazowym
+    std::sort(points.begin() + 1, points.end(), 
+        [&pivot](const Point& a, const Point& b) {
+            float orientationVal = orientation(pivot, a, b);
+            
+            // Jeśli punkty mają ten sam kąt, wybierz bliższy
+            if (orientationVal == 0) {
+                return distance(pivot, a) < distance(pivot, b);
+            }
+            
+            // Sortuj względem kąta (przeciwnie do ruchu wskazówek zegara)
+            return orientationVal > 0;
+        });
+
+    std::vector<Point> hull;
+    hull.push_back(points[0]);
+    hull.push_back(points[1]);
+
+    for (size_t i = 2; i < points.size(); ++i) {
+        // Usuwaj punkty z otoczki, które tworzą skręt w prawo
+        while (hull.size() > 1) {
+            Point top = hull.back();
+            hull.pop_back();
+            
+            if (orientation(hull.back(), top, points[i]) > 0) {
+                hull.push_back(top);
+                break;
+            }
+        }
+        hull.push_back(points[i]);
+    }
+
+    //std::cout << "hull size = " << hull.size() << std::endl;
+    return hull;
+}
 
 int main(int argc, char **argv)
 {
   sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
-
   User user {};
 
   Vec2f mpos { 0.0f, 0.0f };
@@ -161,7 +359,7 @@ int main(int argc, char **argv)
     user.update(mpos);
 
     window.clear(sf::Color::Black);
-    user.drawCurvePoints(&window);
+    user.draw(&window);
     window.display();
   }
 
